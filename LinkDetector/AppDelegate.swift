1e074b67233370bb820cfe688c650b3d1fbda046
Christian Tietze <christian.tietze@gmail.com>
2017-03-17 15:11:07 +0100

    add NSDataDetector-based link finding

--- LinkDetector/AppDelegate.swift
+++ LinkDetector/AppDelegate.swift
@@ -1,27 +1,44 @@
-//
-//  AppDelegate.swift
-//  LinkDetector
-//
-//  Created by Christian Tietze on 17/03/17.
-//  Copyright © 2017 Christian Tietze. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet var textView: NSTextView!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+
+        let text = "Lorem ipsum dolor sit amet, http://example.com/ consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur@sint.occa cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+        let attributedString = autoAddLink(string: text)
+        textView.textStorage?.setAttributedString(attributedString)
+    }
+
+    func autoAddLink(string: String) -> NSAttributedString {
+
+        let attributedString = NSMutableAttributedString(string: string)
+
+        let linkDetector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+
+        linkDetector.enumerateMatches(
+            in: string,
+            options: [],
+            range: NSRange(location: 0, length: (string as NSString).length)) { (textCheckingResult, matchingFlags, stop) in
+
+                guard let textCheckingResult = textCheckingResult else { return }
+
+                if let url = textCheckingResult.url {
+                    let attributes = [NSLinkAttributeName : url]
+                    attributedString.addAttributes(attributes, range: textCheckingResult.range)
+                }
+        }
+
+        return attributedString
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
